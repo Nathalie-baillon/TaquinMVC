b52823d9ab088b9228738ea0c53b07acd0b6b124
Nathalie <nathalie4472@hotmail.fr>
2014-12-14 13:56:22 +0100

    corrected a bug

--- src/Vue/VueGraphique.java
+++ src/Vue/VueGraphique.java
@@ -18,7 +18,7 @@ import Modele.PlateauDeJeu;
 public class VueGraphique extends JFrame implements Observer{
 	private JPanel panel = new JPanel();
 	private JPanel grille = new JPanel();
-	private JButton boutonMelange = new JButton("Mélanger");
+	private JButton boutonMelange = new JButton("MÃ©langer");
 	int nl = 4; //nombre lignes
 	int nc = 4; //nombre colonnes
 	final int tailleDisplayX = 403;
@@ -66,7 +66,7 @@ public class VueGraphique extends JFrame implements Observer{
 
 		panel.add(commandes, BorderLayout.PAGE_START);*/
 
-		// Découpage de l'image et affichage de l'image
+		// DÃ©coupage de l'image et affichage de l'image
 		ImageCutter vi = new ImageCutter("image.jpg", tailleDisplayX,tailleDisplayY);
 		icones = vi.divide(nl, nc);
 		icones[0][0] = null;
@@ -85,7 +85,7 @@ public class VueGraphique extends JFrame implements Observer{
 		}
 		panel.add(grille, BorderLayout.CENTER);	    
 
-		// Bouton de mélange
+		// Bouton de mÃ©lange
 		panel.add(boutonMelange, BorderLayout.PAGE_END);
 
 		return panel;
@@ -93,14 +93,12 @@ public class VueGraphique extends JFrame implements Observer{
 
 
 	public void jeuTermine(){
-		int reponse = p.showConfirmDialog(this, "Bravo! Vous avez terminé le jeu. \n" +
+		int reponse = p.showConfirmDialog(this, "Bravo! Vous avez terminÃ© le jeu. \n" +
 				"Voulez-vous rejouer?","Bravo", JOptionPane.YES_NO_OPTION);
 		if (reponse == JOptionPane.NO_OPTION){
 			System.exit(0);
 		}
 		if (reponse == JOptionPane.YES_OPTION){
-			dispose();
-			VueGraphique vueGraphique = new VueGraphique(new PlateauDeJeu(4,4));
 		}
 	}
 
